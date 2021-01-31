f57504fd94d705a3aa04eefcbee5a66b65e18312
Terra Roush <terra.roush@gmail.com>
2021-01-31 11:53:04 -0600

    added listsummary viewmodel

---
+++ TheList_Capstone/Models/ViewModels/ListSummary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheList_Back_end_Capstone_ServerSide.Models.ViewModels
+{
+    public class ListSummary
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AbbreviatedList { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public int ListTypeId { get; set; }
+        public ListType ListType { get; set; }
+        public string PreviewText => AbbreviatedList + "...";
+    }
+}
---
+++ TheList_Capstone/Repositories/List.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheList_Back_end_Capstone_ServerSide.Repositories
+{
+    public class List
+    {
+    }
+}
