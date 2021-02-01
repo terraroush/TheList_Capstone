1922d82bca5fe1bbb58baf979e2c0172ac07168e
Terra Roush <terra.roush@gmail.com>
2021-01-31 23:26:51 -0600

    added listitem repository, getById

---
+++ TheList_Capstone/Repositories/ListItemRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheList_Capstone.Data;
+using TheList_Capstone.Models;
+
+namespace TheList_Capstone.Repositories
+{
+    public class ListItemRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ListItemRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ListItem GetById(int id)
+        {
+            return _context.ListItem
+                .Include(l => l.Name)
+                .Include(l => l.UserList)
+                .Where(l => l.Id == id)
+                .FirstOrDefault();
+        }
+
+        public List<UserList> GetAll()
+        {
+            return _context.UserList
+                .OrderByDescending(ul => ul.DateCreated)
+                .ToList();
+        }
+
+        public void Add(UserList userList)
+        {
+            userList.DateCreated = DateTime.Now;
+
+            _context.Add(userList);
+            _context.SaveChanges();
+        }
+
+        public void Update(UserList userList)
+        {
+            _context.Entry(userList).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(UserList userList)
+        {
+            _context.UserList.Remove(userList);
+            _context.SaveChanges();
+        }
+    }
+}
