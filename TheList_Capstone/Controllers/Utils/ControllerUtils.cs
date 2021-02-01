a95f1d5a22f74f88d61a1ea878bd0e546694e1dc
Terra Roush <terra.roush@gmail.com>
2021-01-31 18:31:28 -0600

    useList controller is set up

---
+++ TheList_Capstone/Controllers/Utils/ControllerUtils.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TheList_Capstone.Models;
+using TheList_Capstone.Repositories;
+
+namespace TheList_Capstone.Controllers.Utils
+{
+    // this utility method returns an authenticated current user object
+    public class ControllerUtils
+    {
+        public static UserProfile GetCurrentUserProfile(IUserProfileRepository _userProfileRepository, ClaimsPrincipal user)
+        {
+            var firebaseUserId = user.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+    }
+}
--- TheList_Capstone/Repositories/UserProfileRepository.cs
+++ TheList_Capstone/Repositories/UserProfileRepository.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using TheList_Capstone.Data;
 using TheList_Capstone.Models;
 
